5e9037dbca78421eb0b902df194591143fc701a9
cnapop <93379413+cnapop@users.noreply.github.com>
2023-04-10 19:54:00 +0800

    feat: add Player

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,12 +1,18 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
     public String name;
     public boolean win;
-    public ArrayList<AbstractCard> hand;
-    public ArrayList<AbstractCard> bank;
+    public List hand = new ArrayList<AbstractCard> ();
+    public List bank = new ArrayList<AbstractCard> ();
+
+    public Player(String n){
+        name = n;
+        win = false;
+    }
 
     /***
      * Take two card when it's your turn to play. If there aren't enough card for you to draw, return false.
