2990886f869101ab4865dc1f44e4008a6aaf30dd
Sherry <987300027@qq.com>
2023-04-13 21:29:19 +0800

    feat: single color property add current color

--- src/main/java/model/PropertyCard.java
+++ src/main/java/model/PropertyCard.java
@@ -1,8 +1,10 @@
 package model;
 
 public class PropertyCard extends AbstractCard{
-    public PropertyCard(String name, int value, String description) {
+    private Colors currentColor;
+    public PropertyCard(String name, int value, String description,Colors currentColor) {
         super(name, CardTypes.PROPERTY, value, description);
+        this.currentColor=currentColor;
     }
 
     @Override
---
+++ src/main/java/model/SingleColorProperty.java
@@ -0,0 +1,8 @@
+package model;
+
+public class SingleColorProperty extends PropertyCard{
+
+    public SingleColorProperty(String name, int value, String description,Colors currentColor) {
+        super(name, value, description, currentColor);
+    }
+}
