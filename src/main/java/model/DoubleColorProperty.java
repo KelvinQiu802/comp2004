70853518249bd9612c9e75788b82e3eb02217dcf
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-17 13:31:50 +0800

    fix: card description

--- src/main/java/model/DoubleColorProperty.java
+++ src/main/java/model/DoubleColorProperty.java
@@ -5,7 +5,7 @@ public class DoubleColorProperty extends PropertyCard {
     private Colors second;
 
     public DoubleColorProperty(String name, int value, Colors first, Colors second) {
-        super(name, value, "Property Card", first);  // current color is the first color by default
+        super(name, value, "Two Colors Property Wild Card", first);  // current color is the first color by default
         this.first = first;
         this.second = second;
     }
