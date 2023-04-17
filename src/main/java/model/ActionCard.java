ba147b5c3d9e93066927cc174b678f770ee024ae
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-17 10:16:19 +0800

    fix: action card is an abstract class

--- src/main/java/model/ActionCard.java
+++ src/main/java/model/ActionCard.java
@@ -1,14 +1,12 @@
 package model;
 
-public class ActionCard extends AbstractCard implements IBankCard {
+public abstract class ActionCard extends AbstractCard implements IBankCard {
     public ActionCard(String name, int value, String description) {
         super(name, CardTypes.ACTION, value, description);
     }
 
     @Override
-    public void play() {
-
-    }
+    abstract public void play();
 
     /***
      * Drop the card to the center.
