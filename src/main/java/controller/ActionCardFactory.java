92b46bd5d1fb56cbddd01e605dd82bf7a46988ed
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-17 11:36:25 +0800

    fix: static method

--- src/main/java/controller/ActionCardFactory.java
+++ src/main/java/controller/ActionCardFactory.java
@@ -5,7 +5,7 @@ import model.actioncards.ActionCardsName;
 import model.actioncards.DealBreaker;
 
 public class ActionCardFactory {
-    public AbstractCard create(ActionCardsName cardName) {
+    public static AbstractCard create(ActionCardsName cardName) {
         if (cardName == ActionCardsName.DEAL_BREAKER) {
             return new DealBreaker();
         }
