039c24d724964cd13bb6e030f389d45b579698ec
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-17 16:11:47 +0800

    fix: deal cards test

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -8,11 +8,9 @@ import java.util.List;
 
 public class Game {
     private static List<Player> players = new ArrayList<>();
-    private static DrawPile drawPile;
+    private static DrawPile drawPile = new DrawPile(CardFactory.createCards());
 
     public static void main(String[] args) {
-        drawPile = new DrawPile(CardFactory.createCards());
-
         //TODO: Create Player
         players.add(new Player("A"));
         players.add(new Player("B"));
@@ -20,12 +18,20 @@ public class Game {
         dealFiveCardsToAllPlayer();
     }
 
+    /***
+     * Deal any number of cards to a player.
+     * @param player the target
+     * @param num number of cards
+     */
     public static void dealCards(Player player, int num) {
         for (int i = 0; i < num; i++) {
             player.draw(drawPile);
         }
     }
 
+    /***
+     * Deal five cards to every player at the begining of the game.
+     */
     public static void dealFiveCardsToAllPlayer() {
         for (Player player : players) {
             dealCards(player, 5);
--- src/test/java/ControllerTest.java
+++ src/test/java/ControllerTest.java
@@ -1,5 +1,6 @@
 import controller.Game;
 import model.Player;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,6 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ControllerTest {
+    @BeforeEach
+    public void createPlayer() {
+        List<Player> players = Game.getPlayers();
+        players.add(new Player("Kelvin"));
+        players.add(new Player("Jack"));
+    }
+
     @Test
     public void dealFiveCardsToAll() {
         List<Player> players = Game.getPlayers();
