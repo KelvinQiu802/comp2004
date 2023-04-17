9b35ddca624754c68bdbf11d6f73f7fb6a032324
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-17 13:17:40 +0800

    fix: single color property constructor

--- src/main/java/model/SingleColorProperty.java
+++ src/main/java/model/SingleColorProperty.java
@@ -1,7 +1,7 @@
 package model;
 
 public class SingleColorProperty extends PropertyCard {
-    public SingleColorProperty(String name, int value, String description, Colors currentColor) {
-        super(name, value, description, currentColor);
+    public SingleColorProperty(String name, int value, Colors color) {
+        super(name, value, "Property Card", color);
     }
 }
