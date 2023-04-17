46e55ac95585679100fc17e73880933b4164f4d5
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-17 10:54:46 +0800

    feat: action cards name enum

---
+++ src/main/java/model/actioncards/ActionCardsName.java
@@ -0,0 +1,29 @@
+package model.actioncards;
+
+public enum ActionCardsName {
+    DEAL_BREAKER("DEAL BREAKER"),
+    DEBT_COLLECTOR("DEBT COLLECTOR"),
+    DOUBLE_THE_RENT("DOUBLE THE RENT"),
+    FORCED_DEAL("FORCED DEAL"),
+    HOTEL("HOTEL"),
+    HOUSE("HOUSE"),
+    ITS_MY_BIRTHDAY("IT'S MY BIRTHDAY"),
+    JUST_SAY_NO("JUST SAY NO"),
+    PASS_GO("PASS GO"),
+    SLY_DEAL("SLY DEAL");
+
+    private String cardName;
+
+    private ActionCardsName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    @Override
+    public String toString() {
+        return cardName;
+    }
+}
