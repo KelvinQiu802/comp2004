c7e84939590c841b236b15fcbb0b6036bae7c055
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-17 11:10:07 +0800

    fix: typo

--- src/main/java/model/actioncards/MyBirthday.java
+++ src/main/java/model/actioncards/MyBirthday.java
@@ -4,7 +4,7 @@ import model.ActionCard;
 
 public class MyBirthday extends ActionCard {
     public MyBirthday() {
-        super(ActionCardsName.ITS_MY_BIRTHDAY, 2, "All players give you M2m as a gift.");
+        super(ActionCardsName.ITS_MY_BIRTHDAY, 2, "All players give you 2M as a gift.");
     }
 
     @Override
