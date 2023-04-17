68638d79e14920f2dbf930cc533e79965a7c5401
cnapop <93379413+cnapop@users.noreply.github.com>
2023-04-17 10:48:09 +0800

    feat: add two action cards

---
+++ src/main/java/model/actioncards/Hotel.java
@@ -0,0 +1,14 @@
+package model.actioncards;
+
+import model.ActionCard;
+
+public class Hotel extends ActionCard{
+    public Hotel() {
+        super("Hotel", 4, "Add onto any full set you own to add M4m to the rent value." + "(Except railroads and utilities.)");
+    }
+
+    @Override
+    public void play() {
+
+    }
+}
