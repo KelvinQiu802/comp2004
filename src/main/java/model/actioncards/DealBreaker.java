8fd05c6bfeb6dba8943d828a9485311a57906e5a
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-17 10:21:21 +0800

    feat: deal breaker

---
+++ src/main/java/model/actioncards/DealBreaker.java
@@ -0,0 +1,15 @@
+package model.actioncards;
+
+import model.ActionCard;
+
+public class DealBreaker extends ActionCard {
+    public DealBreaker() {
+        super("Deal Breaker", 5, "Steal a complete set of properties from any player. " +
+                "(Includes any buildings.)");
+    }
+
+    @Override
+    public void play() {
+
+    }
+}
