fc7e6d4eb81fd40ad57b069c125a3b59cdfdbf36
Sherry <987300027@qq.com>
2023-04-17 14:49:38 +0800

    feat: dealCards

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,4 +1,30 @@
 package controller;
 
+import model.AbstractCard;
+import model.DrawPile;
+import model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static List<Player> players = new ArrayList<>();
+    private static DrawPile drawPile;
+
+    private static void dealCards(Player player, int num) {
+        for (int i = 0; i < num; i++) {
+            player.draw();
+        }
+    }
+
+    public static void main(String[] args) {
+        players.add(new Player("A"));
+        players.add(new Player("B"));
+        drawPile = new DrawPile(CardFactory.createCards());
+        for(Player player: players){
+            dealCards(player, 5);
+        }
+
+
+    }
 }
