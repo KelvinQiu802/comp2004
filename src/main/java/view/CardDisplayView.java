79cd60e8b21822755852f2df8abf055c5bc52d2b
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-11 14:17:00 +0800

    feat: add description column

--- src/main/java/view/CardDisplayView.java
+++ src/main/java/view/CardDisplayView.java
@@ -22,12 +22,12 @@ public class CardDisplayView {
      * @param cards Cards want to print
      */
     public static void printCard(List<AbstractCard> cards) {
-        String[] header = {"Index", "Name", "Value", "Type"};
-        String[][] data = new String[cards.size()][4];
+        String[] header = {"Index", "Name", "Value", "Type", "Description"};
+        String[][] data = new String[cards.size()][5];
         for (int i = 0; i < cards.size(); i++) {
             AbstractCard card = cards.get(i);
             data[i] = new String[]{String.valueOf(i), card.getName(), String.valueOf(card.getValue()),
-                    card.getType().toString()};
+                    card.getType().toString(), card.getDescription()};
         }
         System.out.println(FlipTable.of(header, data));
     }
