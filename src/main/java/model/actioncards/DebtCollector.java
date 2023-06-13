48bba85834ba591e196f128eea91f107b91d58ef
DreD <564773100@qq.com>
2023-06-13 20:48:30 +0800

    feat: DebtCollector

--- src/main/java/model/actioncards/DebtCollector.java
+++ src/main/java/model/actioncards/DebtCollector.java
@@ -1,7 +1,12 @@
 package model.actioncards;
 
+import controller.Game;
 import model.ActionCard;
 import model.Player;
+import view.PlayerInputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DebtCollector extends ActionCard {
     public DebtCollector() {
@@ -10,6 +15,13 @@ public class DebtCollector extends ActionCard {
 
     @Override
     public void play(Player currentPlayer) {
+        // confirm the player who need to pay
+        List<Player> others = new ArrayList<>(Game.getPlayers());
+        others.remove(currentPlayer);
+        int playerIndex = PlayerInputView.getPlayerIndex(others.size());
+        Player target = others.get(playerIndex);
 
+        // rent
+        target.payTo(currentPlayer, 5);
     }
 }
