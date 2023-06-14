0c78706ad89faa886a991e21b021d45e8dfa176e
cnapop <93379413+cnapop@users.noreply.github.com>
2023-06-14 15:21:34 +0800

    fix: DealBreakerTest

--- src/test/java/DealBreakerTest.java
+++ src/test/java/DealBreakerTest.java
@@ -46,4 +46,18 @@ public class DealBreakerTest {
         assertSame(p2.getPropertyDeck().getPropertySets().get(0), set1);
         PropertyDisplayView.printPropertyDeck(p2.getPropertyDeck());
     }
+
+    @Test
+    public void testUnfilled() {
+        DrawPile pile = new DrawPile(Printer.CardFactory.createCards());
+        PropertySet set1 = new PropertySet(Colors.GREEN);
+        PropertyCard c1 = new SingleColorProperty("1", 1, Colors.GREEN);
+        PropertyCard c2 = new SingleColorProperty("2", 2, Colors.GREEN);
+        set1.add(c1);
+        set1.add(c2);
+        p1.getPropertyDeck().insertSet(set1);
+        dealBreaker.play(p2, list);
+        assertSame(p2.getPropertyDeck().getPropertySets().get(0), set1);
+        PropertyDisplayView.printPropertyDeck(p2.getPropertyDeck());
+    }
 }
