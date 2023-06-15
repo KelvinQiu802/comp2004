be9f4550639b05c34b7e82c2ced55d4a565e5263
DreD <564773100@qq.com>
2023-06-15 17:11:39 +0800

    feat: CardUtils

--- src/main/java/utils/CardUtils.java
+++ src/main/java/utils/CardUtils.java
@@ -6,6 +6,12 @@ import model.PropertyDeck;
 import model.PropertySet;
 
 public class CardUtils {
+    /***
+     * Get property card by index
+     * @param pd
+     * @param target
+     * @return card
+     */
     public static IPropertyCard getPropertyCardByIndex(PropertyDeck pd, int target) {
         int count = 0;
         for (PropertySet ps : pd.getPropertySets()) {
@@ -19,6 +25,12 @@ public class CardUtils {
         return null;
     }
 
+    /***
+     * Get property card by index in property set
+     * @param ps
+     * @param target
+     * @return card
+     */
     public static IPropertyCard getPropertyCardByIndexInPropertySet(PropertySet ps, int target) {
         int count = 0;
         for (IPropertyCard card : ps.getProperties()) {
@@ -30,6 +42,12 @@ public class CardUtils {
         return null;
     }
 
+    /***
+     * Check if it is same color unfilled set
+     * @param pd
+     * @param color
+     * @return true or false
+     */
     public static boolean IsSameColorUnfilledSet(PropertyDeck pd, Colors color) {
         for (PropertySet set : pd.getPropertySets()) {
             if (set.getColor() == color && !set.isFullSet())
